312a89bca1bc2a9256146a647793dec32747c62b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-07 17:51:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const pathUI = "/ui"
 const pathToPage = 'https://shaman2016scratch.github.io/p8787Lang'
 
 async function start() {
-  const LanguageVM = await (await fetch(`${pathToPage}${scriptsPath}${pathVm}/index.js`)).json()
-  const LanguageRunner = await (await fetch(`${pathToPage}${scriptsPath}${pathRunner}/index.js`)).json()
-  const RunnerUI = await (await fetch(`${pathToPage}${scriptsPath}${pathUI}/index.js`)).json()
+  const LanguageVM = await (await fetch(`${pathToPage}${scriptsPath}${pathVm}/index.js`)).text()
+  const LanguageRunner = await (await fetch(`${pathToPage}${scriptsPath}${pathRunner}/index.js`)).text()
+  const RunnerUI = await (await fetch(`${pathToPage}${scriptsPath}${pathUI}/index.js`)).text()
+  let functions = {
+    vm: new Function(['params'], LanguageVM),
+    runner: new Function(['params'], LanguageRunner),
+    ui: new Function(['params'], RunnerUI)
+  }
 }
