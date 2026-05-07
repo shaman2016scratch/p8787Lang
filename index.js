cec823dc572b05094cc66e77f6594e279fc29796
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-07 17:41:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const scriptsPath = '/js'
 const pathRunner = "/play"
 const pathVm = "/vm"
+const pathUI = "/ui"
 const pathToPage = 'https://shaman2016scratch.github.io/p8787Lang'
 
 async function start() {
   const LanguageVM = await (await fetch(`${pathToPage}${scriptsPath}${pathVm}/index.js`)).json()
   const LanguageRunner = await (await fetch(`${pathToPage}${scriptsPath}${pathRunner}/index.js`)).json()
+  const RunnerUI = await (await fetch(`${pathToPage}${scriptsPath}${pathUI}/index.js`)).json()
 }
