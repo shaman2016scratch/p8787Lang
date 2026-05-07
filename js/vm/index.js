d601c3198112157aadfc9180dc7419b626a562f6
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-07 22:56:29 +0300

    Update index.js

--- js/vm/index.js
+++ js/vm/index.js
@@ -1,22 +1,25 @@
-let exported = {}
+async function VM() {
+  let exported = {}
 
-const metadata = {
-  name: 'VM8787_1',
-  version: '0.0.1',
-  developers: [
-    {
-      name: 'polzovatel_8787',
-      links: []
-    }
-  ],
-  license: 'MIT License',
-  description: ''
-}
+  const metadata = {
+    name: 'VM8787_1',
+    version: '0.0.1',
+    developers: [
+      {
+        name: 'polzovatel_8787',
+        links: []
+      }
+    ],
+    license: 'MIT License',
+    description: ''
+  }
 
-exported = {
-  metadata
-}
+  exported = {
+    metadata
+  }
 
-return {
-  export: exported
+  return {
+    export: exported
+  }
 }
+return VM()
