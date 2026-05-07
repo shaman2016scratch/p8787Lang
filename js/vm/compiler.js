546cc40bc43a49017d982c15b39c48a5cd988514
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-07 23:05:54 +0300

    Update compiler.js

--- js/vm/compiler.js
+++ js/vm/compiler.js
@@ -1,1 +1,28 @@
-
+async function COMPILER() {
+  let exported = {}
+  async function compile(code) {
+    let compiled = {
+      code: []
+    }
+    let i = code.split('\n')
+    for(let i2 = 0; i2 < i.length; i2++) {
+      let compiledLine = {}
+      let i3 = i[i2]
+      let i4 = i3.split(' ')
+      if (i4[0] === 'load') {
+        compiledLine = {
+          operation: 'load',
+          item: i4[1],
+          value: i4[2]
+        }
+      }
+      compiled.code.push(compiledLine)
+    }
+    return compiled
+  }
+  exported = {
+    compile
+  }
+  return exported
+}
+return COMPILER()
